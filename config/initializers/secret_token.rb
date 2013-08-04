4974a606717e6e5382f83725b6261ba2c851649d
mahmoud metwally <mmetwally@cloud9ers.com>
2013-08-04 12:52:15 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiliconVesion::Application.config.secret_token = '41d2777f9f817896bf4714c4099cabb410c1d788058af8cedfb45b19dcd663b0e5e8a0d66338ee9cc0df5a553a0ec0a97b2cbd12e186f0f6e5b3cc615cc86fc7'
