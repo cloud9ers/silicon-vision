cfe6f4f55ae96f69df54db39b4e7cb883f45f96a
Amal Elshihaby <ashihaby@cloud9ers.com>
2013-08-06 14:02:03 +0200

    Clean the repo

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 SiliconVesion::Application.routes.draw do
-  devise_for :users
-  resources :users
-  authenticated :user do
-    root :to => 'home#index'
-  end
   root :to => "home#index"
   match ':action' => 'home#:action'
   match 'ips/:controller/:action' => ':controller#:action' 
