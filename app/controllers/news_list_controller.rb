74c52c93d3c909ad6fad880f4579bd6185edc064
Khaled ElAdawy <keladawy@asoliman.sw.in>
2013-10-07 12:55:05 +0200

    Added a file

---
+++ app/controllers/news_list_controller.rb
@@ -0,0 +1,22 @@
+class PowerManagementController < ApplicationController
+  define_method('index') do
+  end
+  
+  define_method('band-gap-reference') do
+  end
+  
+  define_method('dc-dc-buck-converters') do
+  end
+
+  define_method('temperature-sensors') do
+  end
+
+  define_method('por-supply-monitors') do
+  end
+
+  define_method('linear-chargers') do
+  end
+
+  define_method('loe-drop-out-regulators') do
+  end
+end
