40f3726286ae0d3d9f1c2c8aab668502b9e7e223
λhmad λjmi <ahmadajmi@outlook.com>
2014-07-02 15:01:00 +0200

    Removed the radio_frequency controller and replaced it with a new controller called IpsController that will hold all the products so it will be the top branch Changed the route for the IP link in the header to route to the new IpsController index

---
+++ app/controllers/ips_controller.rb
@@ -0,0 +1,18 @@
+class IpsController < ApplicationController
+
+  define_method('index') do
+  end
+
+  define_method('bluetooth-low-energy') do
+  end
+
+  define_method('sub-1ghz') do
+  end
+
+  define_method('zwave') do
+  end
+
+  define_method('zigbee') do
+  end
+
+end
